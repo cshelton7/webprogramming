244a35d92f066a338c44fbaf4ad72f2ba8571698
cshelton7 <cshelton7@student.gsu.edu>
2022-02-03 03:48:54 -0500

    new php file for week4

---
+++ week4.php
@@ -0,0 +1,5 @@
+<php>
+
+## This is actually just from the video lol
+<script src="C:\Users\clshe\Anaconda3\pkgs\sphinx-4.0.1-pyhd3eb1b0_0\site-packages\sphinx\themes\basic\static"></script>
+</php>
